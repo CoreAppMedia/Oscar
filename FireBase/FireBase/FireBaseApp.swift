459c523c5870a9c4918fde9529925aeccc3262dc
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-10-18 12:34:43 -0600

    inicio de App
    en esta aplicacion veremos todo lo relacionado con firebase

---
+++ FireBase/FireBase/FireBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseApp.swift
+//  FireBase
+//
+//  Created by Oscar Valdes on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
