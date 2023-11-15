60c833a68d51cf83fe325f20d4adb097d277479a
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-11-15 13:07:35 -0600

    a
    a

--- ChatGPT4/ChatGPT4/ContentView.swift
+++ ChatGPT4/ChatGPT4/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    var viewModel = ViewModel()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button{
+                Task{
+                    await viewModel.send()
+                }
+            }label: {
+                Image(systemName: "paperplane.fill")
+                    .foregroundColor(Color.white)
+                    .frame(width: 44,height: 44)
+                    .background(Color.blue)
+                    .cornerRadius(22)
+                
+            }
         }
-        .padding()
+        .padding(.leading,8)
     }
 }
 
---
+++ ChatGPT4/ChatGPT4/ViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  ViewModel.swift
+//  ChatGPT4
+//
+//  Created by Oscar Valdes on 15/11/23.
+//
+
+import Foundation
+import SwiftOpenAI
+
+final class ViewModel {
+    var openAI = SwiftOpenAI(apiKey: "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")
+    func send() async {
+        //creamos una red de mensajes
+        let messages: [MessageChatGPT] = [
+            MessageChatGPT(text: "Eres un asistente que responde preguntas de IOS", role: .system),
+            MessageChatGPT(text: "¿Cuando se lanzo el primer iphone?", role: .user)
+        ]
+        //son los parametros para realizar nuestra peticion http://
+        let optionalParameters = ChatCompletionsOptionalParameters(temperature: 0.7, stream: true, maxTokens: 50)
+        
+        do {
+            //aqui instanciamos como queremos que con conteste ChatGPT, en este caso con mensajes
+            let stream = try await openAI.createChatCompletionsStream(model: .gpt4(.base), messages: messages, optionalParameters: optionalParameters)
+            
+            for try await response in stream {
+                print(response)
+            }
+        } catch {
+            print("Error master: \(error)")
+        }
+    }
+}
