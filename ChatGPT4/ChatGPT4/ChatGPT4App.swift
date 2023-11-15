3466ecfd52448d39085d2f590e1bf94205ee2405
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-11-15 11:23:25 -0600

    ChatGPT4
    nuevo programa para chatear con IA

---
+++ ChatGPT4/ChatGPT4/ChatGPT4App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT4App.swift
+//  ChatGPT4
+//
+//  Created by Oscar Valdes on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPT4/ChatGPT4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatGPT4
+//
+//  Created by Oscar Valdes on 15/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
